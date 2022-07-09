9105fe3810da91f68db2666279e828876a2c9399
marta <ma.moscati@gmail.com>
2022-07-09 14:50:44 +0200

    Remove file if exists in separate_simple_track.py

--- tests/test_basic_pitch.py
+++ tests/test_basic_pitch.py
@@ -1,34 +1,34 @@
 import os
 from pathlib import Path
-import sys
 
 from basic_pitch import ICASSP_2022_MODEL_PATH
+#from basic_pitch.inference import predict_and_save
+from inference import predict_and_save
 
-print(sys.executable)
-from basic_pitch.inference import predict_and_save
+SONG_NAME = 'test'
+RES_DIR = Path(os.environ["TSOAI_HACK_LULLABY_RESOURCES"])
+SONG_PATH = RES_DIR / SONG_NAME / 'vocals.wav'
+OUTPUT_PATH = RES_DIR / 'vocals_basic_pitch.mid'
 
+if __name__ == '__main__':
+    if os.path.exists(OUTPUT_PATH):
+        os.remove(OUTPUT_PATH)
 
-SONG_NAME = '0009fFIM1eYThaPg'
-RES_DIR=Path(os.environ["TSOAI_HACK_LULLABY_RESOURCES"])
-SONG_PATH = RES_DIR/SONG_NAME/'vocals.wav'
-OUTPUT_PATH = RES_DIR
-
-
-predict_and_save(
-# mandatory parameters
-    audio_path_list= [str(SONG_PATH),],
-    output_directory=str(OUTPUT_PATH),
-    save_midi=True,
-    sonify_midi=False,
-    save_model_outputs=False,
-    save_notes=False,
-# optional parameters
-    model_path=ICASSP_2022_MODEL_PATH,
-    onset_threshold=0.5,
-    frame_threshold=0.3,
-    minimum_note_length=58,
-    minimum_frequency=None,
-    maximum_frequency=None,
-    melodia_trick=True,
-    debug_file=None,
-)
+    predict_and_save(
+        # mandatory parameters
+        audio_path_list=[str(SONG_PATH), ],
+        output_directory=str(RES_DIR),
+        save_midi=True,
+        sonify_midi=False,
+        save_model_outputs=False,
+        save_notes=False,
+        # optional parameters
+        model_path=ICASSP_2022_MODEL_PATH,
+        onset_threshold=0.6,
+        frame_threshold=0.3,
+        minimum_note_length=100,
+        minimum_frequency=None,
+        maximum_frequency=None,
+        melodia_trick=True,
+        debug_file=None,
+    )
