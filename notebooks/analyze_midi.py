cabf1e15eec3c31d2bff932a4bcf6c69ab4564db
Michele Buccoli <m.buccoli@gmail.com>
2022-07-10 12:22:09 +0200

    code with comments

--- notebooks/analyze_midi.py
+++ notebooks/analyze_midi.py
@@ -78,19 +78,27 @@ plt.plot(t,np.abs(re_synth_slow_lp[i_l:i_r]))
 
 # %%
 noise_pars={
-    "SNR_range":[-30, -18],
-    "dur_range":[3, 10],
-    "overlap_secs":[0.2, 1],
-    "lp_factor_range":[0.01, 0.2],
+    "SNR_range":[-30, -18], # what's the SNR w.r.t. the lullaby?
+    "dur_range":[3, 10], # duration of each noise wave
+    "overlap_secs":[0.2, 1], # second of overlap between two waves
+    "lp_factor_range":[0.01, 0.2], # rates for low-pass filters 
 }
-lullaby_lp_reverb, sr=sf.read(fn_out)
-energy=np.std(lullaby_lp_reverb)
-white_noise_array=np.zeros(lullaby_lp_reverb.size)
-k=0
+lullaby_lp_reverb, sr=sf.read(fn_out) # reading the file 
+energy=np.std(lullaby_lp_reverb) #computing the energy (sort of)
+white_noise_array=np.zeros(lullaby_lp_reverb.size) # zeroes with the array
+k=0 # starting with the first sample
 def rand_N(range_x, N):
+    """
+    Dummy function to retrieve a random number between two extremes with a give
+    precision.
+
+    range_x is a list with lower and higher extreme
+    N is the precision, e.g. N=10 will give a 0.1 sensibility 
+    """
     return np.random.randint(range_x[0]*N, range_x[1]*N)/N
 
 while k < white_noise_array.size:
+    # computing random parameter
     dur_noise=rand_N(noise_pars["dur_range"],1000) 
     lp_rate=rand_N(noise_pars["lp_factor_range"],10000)
     target_SNR_dB=rand_N(noise_pars["SNR_range"],10)
@@ -98,17 +106,29 @@ while k < white_noise_array.size:
     overlap_sample=int(overlap_sec*sr)
     print(dur_noise, target_SNR_dB, overlap_sec)
 
+    # generate the noise with 1 second more of length 
+    # I am directly applying lowpass, so removing the first 1second will
+    # avoid smooth transition
     noise_i=lp(np.random.normal(0, 1, int((1+dur_noise)*sr)), lp_rate)
-    noise_i=noise_i[sr:] # add one second to avoid smoothing entrance
+    noise_i=noise_i[sr:] 
+
     N=noise_i.size
-    ampl=np.sin(np.linspace(0,np.pi, N))    
+    ampl=np.sin(np.linspace(0,np.pi, N))  # sine between 0 to 1 and back to 0 in N samples
     noise_i=noise_i*ampl
-    energy_noise=np.std(noise_i)
-    cur_SNR_dB=20*np.log10(energy/energy_noise)
+
+    # samples will be betweek k and k+max_N
+    max_N=min(white_noise_array.size-k, noise_i.size)
+    
+    # computing gain to scale noise to a given snr
+    energy_noise=np.std(noise_i[:max_N])
+    energy_sig=np.std(lullaby_lp_reverb[k:k+max_N])
+
+    cur_SNR_dB=20*np.log10(energy_sig/energy_noise)
     diff_SNR = np.power(10, (target_SNR_dB - cur_SNR_dB)/20)
     #assert diff_SNR<1, "Problem"
     noise_i=noise_i*diff_SNR
-    max_N=min(white_noise_array.size-k, noise_i.size)
+    
+    # summing the noise_i component
     white_noise_array[k:k+max_N]=white_noise_array[k:k+max_N]+noise_i[:max_N]
     k+=noise_i.size-overlap_sample
 
