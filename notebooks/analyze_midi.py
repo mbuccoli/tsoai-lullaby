aa4404ff01f306e57487a6af369650ea0625a02e
Michele Buccoli <m.buccoli@gmail.com>
2022-07-10 12:13:36 +0200

    adding white_noise generator, need to explain it

--- notebooks/analyze_midi.py
+++ notebooks/analyze_midi.py
@@ -57,11 +57,12 @@ sf.write(RES_DIR/"test_midi"/"mid_synth_samples_slow.wav", to_int16(re_synth_slo
 
 # %%
 rate = 0.1
-b= [rate]
-a= [1,rate-1]
+def lp(x,rate):
+    b= [rate]
+    a= [1,rate-1]
+    return lfilter(b, a, x)
 
-
-re_synth_slow_lp=lfilter(b, a, re_synth_slow)
+re_synth_slow_lp=lp(re_synth_slow, rate)
 sf.write(RES_DIR/"test_midi"/"lullaby_lp.wav", to_int16(re_synth_slow_lp, 0.7), sr)
 
 # %%
@@ -69,7 +70,6 @@ fn_in=RES_DIR/"test_midi"/"lullaby_lp.wav"
 fn_out=RES_DIR/"test_midi"/"lullaby_reverb.wav"
 os.system(f"sox {fn_in} {fn_out} reverb 75 70 75 100 25 3")
 
-# %%
 i_l=int(1.75*sr)
 i_r=int(1.85*sr)
 t=np.arange(re_synth_slow[i_l:i_r].size)/sr
@@ -77,11 +77,42 @@ plt.plot(t,np.abs(re_synth_slow[i_l:i_r]), lw=5)
 plt.plot(t,np.abs(re_synth_slow_lp[i_l:i_r]))
 
 # %%
+noise_pars={
+    "SNR_range":[-30, -18],
+    "dur_range":[3, 10],
+    "overlap_secs":[0.2, 1],
+    "lp_factor_range":[0.01, 0.2],
+}
+lullaby_lp_reverb, sr=sf.read(fn_out)
+energy=np.std(lullaby_lp_reverb)
+white_noise_array=np.zeros(lullaby_lp_reverb.size)
+k=0
+def rand_N(range_x, N):
+    return np.random.randint(range_x[0]*N, range_x[1]*N)/N
 
-piano_roll=full_data.get_piano_roll()
-plt.imshow(piano_roll[80:40:-1,:1700], aspect="auto", interpolation="none")
+while k < white_noise_array.size:
+    dur_noise=rand_N(noise_pars["dur_range"],1000) 
+    lp_rate=rand_N(noise_pars["lp_factor_range"],10000)
+    target_SNR_dB=rand_N(noise_pars["SNR_range"],10)
+    overlap_sec=rand_N(noise_pars["overlap_secs"],10)
+    overlap_sample=int(overlap_sec*sr)
+    print(dur_noise, target_SNR_dB, overlap_sec)
 
+    noise_i=lp(np.random.normal(0, 1, int((1+dur_noise)*sr)), lp_rate)
+    noise_i=noise_i[sr:] # add one second to avoid smoothing entrance
+    N=noise_i.size
+    ampl=np.sin(np.linspace(0,np.pi, N))    
+    noise_i=noise_i*ampl
+    energy_noise=np.std(noise_i)
+    cur_SNR_dB=20*np.log10(energy/energy_noise)
+    diff_SNR = np.power(10, (target_SNR_dB - cur_SNR_dB)/20)
+    #assert diff_SNR<1, "Problem"
+    noise_i=noise_i*diff_SNR
+    max_N=min(white_noise_array.size-k, noise_i.size)
+    white_noise_array[k:k+max_N]=white_noise_array[k:k+max_N]+noise_i[:max_N]
+    k+=noise_i.size-overlap_sample
+
+sf.write(RES_DIR/"test_midi"/"lullaby_lp_rev_sea.wav", 
+        to_int16(lullaby_lp_reverb+white_noise_array, 0.7), sr)
 
 # %%
-
-
