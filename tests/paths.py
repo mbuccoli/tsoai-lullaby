83c1780d19eb16b0d3ff5b55a123d360da98f1ef
Vincent <vincent831106@gmail.com>
2022-08-17 08:57:06 +0800

    modify path variable and add test code

---
+++ tests/paths.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import os
+
+SONG_NAME = 'baby_shark_cut'
+EXTENSION = '.wav'
+SONG_FILE = SONG_NAME + EXTENSION
+# RES_DIR = Path(os.environ["TSOAI_HACK_LULLABY_RESOURCES"])
+# RES_DIR = Path(os.getcwd()).parent.absolute() / 'resources'
+# sys.path.append(str(Path('./').cwd().parent / 'DataPrepare'))
+# RES_DIR = Path(os.path.dirname(os.path.realpath(__file__).parent())) / 'resources'
+RES_DIR = Path('./').cwd() / 'resources'
+SONG_DIR = RES_DIR / SONG_NAME
+SONG_FILE = SONG_DIR / SONG_FILE
+LULLABY_LP = SONG_DIR / "lullaby_lp.wav"
+LULLABY_LP_REV = SONG_DIR / "lullaby_lp_rev.wav"
+VOCALS_WAV_PATH = SONG_DIR / 'vocals.wav'
+VOCALS_MIDI_PATH = SONG_DIR / 'vocals_basic_pitch.mid'
+SLOW_VOCALS_MIDI_PATH = SONG_DIR / 'slow.wav'
+
